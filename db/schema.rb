5aeedfc959d8d670cb28a38cbe09a409d9cee775
Errin Larsen <errinlarsen@gmail.com>
2014-12-24 02:44:29 -0600

    [v2.3.rc1] update schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130227205845) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "contexts", force: true do |t|
     t.string   "name",                                     null: false
     t.integer  "position"
@@ -127,8 +130,8 @@ ActiveRecord::Schema.define(version: 20130227205845) do
     t.integer  "every_count"
     t.integer  "weekday"
     t.datetime "completed_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
     t.boolean  "show_always"
   end
 
